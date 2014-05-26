4fb35b3955f1bd0b9d318ff9e95792496d05fdbd
Andrew Yager <andrew@rwts.com.au>
2014-05-26 23:59:56 +1000

    Add handler for portal redirect in

--- wlchotspot/hotspot/urls.py
+++ wlchotspot/hotspot/urls.py
@@ -4,5 +4,6 @@ from hotspot import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.HomePageView.as_view(), name='hotspot.index'),
-	url(r'^help$', views.HelpPageView.as_view(), name='hotspot.help')
+	url(r'^help$', views.HelpPageView.as_view(), name='hotspot.help'),
+	url(r'^redirect$', views.redirectLogin, name='hotspot.redirect'),
 )
--- wlchotspot/hotspot/views.py
+++ wlchotspot/hotspot/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
 
 from django.views.generic import FormView
@@ -8,6 +8,14 @@ from django.contrib import messages
 class HomePageView(TemplateView):
 	template_name='hotspot/index.html'
 
+def redirectLogin(request):
+	""" Handler to receive the initial request from the WLC """
+	request.session['portal_ip'] = request.GET['portal_ip']
+	request.session['client_id'] = request.GET['client_id']
+	request.session['ssid'] = request.GET['ssid']
+	request.session['bssid'] = request.GET['bssid']
+	request.session['wbaredirect'] = request.GET['wbaredirect']
+	redirect('hotspot.index')
 
 class HelpPageView(TemplateView):
 	template_name='hotspot/help.html'
